f31ed39f5473e86fc133c3f61ada6bbc3cf75552
Harsh <itsharsh222@gmail.com>
2024-07-24 12:10:40 +0530

    m

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,7 +16,7 @@ function Auth() {
       // api request to login or register
 
       const { data } = await axios.post(
-        `http://localhost:4000/api/users${isRegister ? "" : "/login"}`,
+        `https://mern-backend-b8g7.onrender.com/api/users${isRegister ? "" : "/login"}`,
         { user: values }
       );
 
